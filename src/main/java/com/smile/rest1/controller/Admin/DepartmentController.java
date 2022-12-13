58cf4122976560e2fcc9f724f818a40615c23e0a
Aditya Brillio <aditya.patil1@brillio.com>
2022-12-13 12:18:34 +0530

    Department controller CORS fix

--- src/main/java/com/smile/rest1/controller/Admin/DepartmentController.java
+++ src/main/java/com/smile/rest1/controller/Admin/DepartmentController.java
@@ -3,6 +3,7 @@ package com.smile.rest1.controller.Admin;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smile.rest1.dao.DepartmentRepo;
 import com.smile.rest1.model.Department;
 
+@CrossOrigin()
 @RestController
 @RequestMapping("/admin/department")
 public class DepartmentController {
